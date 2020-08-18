123c362e7e44cbb22f80059036d1475eb2e23fb2
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-08-18 15:08:28 -0500

    added searchbar and its setstate functionality to the container jsx

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -2,73 +2,70 @@ import React from "react";
 import "../Styles/Container.css";
 import Api from "../utils/Api";
 import Table from "./Table.jsx";
-
+import Searchbar from "./Searchbar";
 
 class Container extends React.Component {
-    state = {
-        employees: [],
-        search: ""
-    }
-    componentDidMount(){
-        Api.search()
-        .then((data)=>{
-            // console.log(data.data.results);
-            let employeeData = data.data.results.map((dataRow) => {
-                return({
-                    id: dataRow.login.uuid,
-                    image: dataRow.picture.thumbnail,
-                    name: dataRow.name.first + " " + dataRow.name.last,
-                    phone: dataRow.phone,
-                    email: dataRow.email,
-                    dob: dataRow.dob.date
-                });
-            })
-            this.setState({employees: employeeData});
-            console.log(this.state.employees);
-        })
-        .catch((err)=> {
-            console.log("Error", err);
+  state = {
+    employees: [],
+    search: "",
+  };
+  componentDidMount() {
+    Api.search()
+      .then((data) => {
+        // console.log(data.data.results);
+        let employeeData = data.data.results.map((dataRow) => {
+          return {
+            id: dataRow.login.uuid,
+            image: dataRow.picture.thumbnail,
+            name: dataRow.name.first + " " + dataRow.name.last,
+            phone: dataRow.phone,
+            email: dataRow.email,
+            dob: dataRow.dob.date,
+          };
         });
-    }
-    render(){
-        return (
-            <div className="container">
-                <div className="wrapper">
-                    <Table>
-                        {this.state.employees ? (
-                            this.state.employees.map(emp => (
-                                <tr className="userData" key={emp.id}>
-                                    <td>
-                                        <img src={emp.image} alt=""/>
-                                    </td>
-                                    <td>
-                                        {emp.name}
-                                    </td>
-                                    <td>
-                                        {emp.phone}
-                                    </td>
-                                    <td>
-                                        {emp.email}
-                                    </td>
-                                    <td>
-                                        {emp.dob}
-                                    </td>
-                                </tr>
-                            ))
-                        ) : (
-                            <tr>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                            </tr>
-                        )}
-                    </Table>
-                </div>
-            </div>
-        );
-    }
+        this.setState({ employees: employeeData });
+        console.log(this.state.employees);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }
+  searchChange = event => {
+    this.setState({search: event.target.value});
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <Searchbar search={this.state.search} searchChange={this.searchChange}/>
+          <p>{this.state.search}</p>
+          <Table>
+            {this.state.employees ? (
+              this.state.employees.map((emp) => (
+                <tr className="userData" key={emp.id}>
+                  <td>
+                    <img src={emp.image} alt="" />
+                  </td>
+                  <td>{emp.name}</td>
+                  <td>{emp.phone}</td>
+                  <td>{emp.email}</td>
+                  <td>{emp.dob}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+                <td>-</td>
+              </tr>
+            )}
+          </Table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Container;
--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+function Searchbar(props){
+    return (
+        <input
+        type="text"
+        placeholder="search here"
+        value={props.search}
+        onChange={props.searchChange}/>
+    )
+}
+
+export default Searchbar;
