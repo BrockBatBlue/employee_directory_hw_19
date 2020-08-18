a2c6dd88234054ce8abbf37cc7b444e3f0b5f037
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-08-18 14:33:43 -0500

    created table jsx and rendered the table data in the container jsx

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,15 +1,68 @@
 import React from "react";
 import "../Styles/Container.css";
+import Api from "../utils/Api";
+import Table from "./Table.jsx";
 
 
 class Container extends React.Component {
-    state = {}
-    componentDidMount(){}
+    state = {
+        employees: [],
+        search: ""
+    }
+    componentDidMount(){
+        Api.search()
+        .then((data)=>{
+            let employeeData = data.data.results.map((dataRow) => {
+                return({
+                    image: dataRow.picture.thumbnail,
+                    name: dataRow.name.first + " " + dataRow.name.last,
+                    phone: dataRow.phone,
+                    email: dataRow.email,
+                    dob: dataRow.dob.date
+                });
+            })
+            this.setState({employees: employeeData});
+            console.log(this.state.employees);
+        })
+        .catch((err)=> {
+            console.log("Error", err);
+        });
+    }
     render(){
         return (
             <div className="container">
                 <div className="wrapper">
-                    <p>This is where stuff goes</p>
+                    <Table>
+                        {this.state.employees ? (
+                            this.state.employees.map(emp => (
+                                <tr>
+                                    <td>
+                                        <img src={emp.image} alt=""/>
+                                    </td>
+                                    <td>
+                                        {emp.name}
+                                    </td>
+                                    <td>
+                                        {emp.phone}
+                                    </td>
+                                    <td>
+                                        {emp.email}
+                                    </td>
+                                    <td>
+                                        {emp.dob}
+                                    </td>
+                                </tr>
+                            ))
+                        ) : (
+                            <tr>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                                <td>-</td>
+                            </tr>
+                        )}
+                    </Table>
                 </div>
             </div>
         );
